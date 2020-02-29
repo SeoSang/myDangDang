5aabee6f9efbaf85ac77fbbb59c2afd12b32cb86
SeoSang <ddrrpg@naver.com>
2020-03-01 02:12:56 +0900

    회원가입틀 수정

--- frontend/components/SignupForm.tsx
+++ frontend/components/SignupForm.tsx
@@ -1,215 +1,230 @@
-import React, { Component, memo, useState } from "react"
-import styled, { css } from "styled-components"
+import React, { useState } from "react"
+import {
+  Form,
+  Input,
+  Tooltip,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  Button,
+  AutoComplete,
+} from "antd"
+import { QuestionCircleOutlined } from "@ant-design/icons"
 
-import { Form, Input, Tooltip, Cascader, Select, Checkbox, Button } from "antd"
+import { Mention } from "@ant-design/compatible"
 
 const { Option } = Select
-const StyledForm = styled.div`
-  border-radius: 15px;
-  background-color: white;
-  border-radius: 10px;
-  padding: 5% 20% 0 3%;
-`
-
-const isSigningUp = false
+const AutoCompleteOption = AutoComplete.Option
 
 const residences = [
   {
-    value: "마포구",
-    label: "마포구",
+    value: "zhejiang",
+    label: "Zhejiang",
     children: [
       {
-        value: "성산동",
-        label: "성산동",
-      },
-      {
-        value: "망원동",
-        label: "망원동",
+        value: "hangzhou",
+        label: "Hangzhou",
+        children: [
+          {
+            value: "xihu",
+            label: "West Lake",
+          },
+        ],
       },
     ],
   },
   {
-    value: "기타",
-    label: "기타",
+    value: "jiangsu",
+    label: "Jiangsu",
+    children: [
+      {
+        value: "nanjing",
+        label: "Nanjing",
+        children: [
+          {
+            value: "zhonghuamen",
+            label: "Zhong Hua Men",
+          },
+        ],
+      },
+    ],
   },
 ]
 
-const SignupForm = ({ form }) => {
-  const [confirmDirty, setConfirmDirty] = useState(false)
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    form.validateFieldsAndScroll((err, values) => {
-      if (!err && !!values.agreement) {
-      }
-    })
-    const formData = form.getFieldsValue()
-  }
-
-  const handleConfirmBlur = e => {
-    const { value } = e.target
-    setConfirmDirty(confirmDirty || !!value)
-  }
-
-  const compareToFirstPassword = (rule, value, callback) => {
-    if (value && value !== form.getFieldValue("password")) {
-      callback("Two passwords that you enter is inconsistent!")
-    } else {
-      callback()
-    }
-  }
-
-  const validateToNextPassword = (rule, value, callback) => {
-    if (value && confirmDirty) {
-      form.validateFields(["confirm"], { force: true })
-    }
-    callback()
-  }
-
-  const validateChecked = (rule, value, callback) => {
-    if (value === true) {
-      form.validateFields(["agreement"], { force: true })
-    }
-    callback()
-  }
-
-  const { getFieldDecorator } = form
-
-  const formItemLayout = {
-    labelCol: {
-      xs: { span: 24 },
-      sm: { span: 8 },
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 6 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 14 },
+  },
+}
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
     },
-    wrapperCol: {
-      xs: { span: 24 },
-      sm: { span: 16 },
+    sm: {
+      span: 16,
+      offset: 8,
     },
+  },
+}
+
+const SignupForm = () => {
+  const [form] = Form.useForm()
+
+  const onFinish = values => {
+    console.log("Received values of form: ", values)
   }
-  const tailFormItemLayout = {
-    wrapperCol: {
-      xs: {
-        span: 24,
-        offset: 0,
-      },
-      sm: {
-        span: 16,
-        offset: 8,
-      },
-    },
-  }
-  const prefixSelector = getFieldDecorator("prefix", {
-    initialValue: "82",
-  })(
-    <Select style={{ width: 70 }}>
-      <Option value='82'>+82</Option>
-    </Select>,
+
+  const prefixSelector = (
+    <Form.Item name='prefix' noStyle>
+      <Select style={{ width: 70 }}>
+        <Option value='82'>+82</Option>
+      </Select>
+    </Form.Item>
   )
 
+  const [autoCompleteResult, setAutoCompleteResult] = useState([])
+
+  const onWebsiteChange = value => {
+    if (!value) {
+      setAutoCompleteResult([])
+    } else {
+      setAutoCompleteResult([".com", ".org", ".net"].map(domain => `${value}${domain}`))
+    }
+  }
+
+  const websiteOptions = autoCompleteResult.map(website => ({
+    label: website,
+    value: website,
+  }))
+
   return (
-    <>
-      <StyledForm>
-        <Form
-          {...formItemLayout}
-          onSubmit={handleSubmit}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-        >
-          <Form.Item label='E-mail'>
-            {getFieldDecorator("email", {
-              rules: [
-                {
-                  type: "email",
-                  message: "올바른 이메일 주소가 아닙니다!",
-                },
-                {
-                  required: true,
-                  message: "이메일을 입력해주세요!",
-                },
-              ],
-            })(<Input />)}
-          </Form.Item>
-          <Form.Item label='비밀번호' hasFeedback>
-            {getFieldDecorator("password", {
-              rules: [
-                {
-                  required: true,
-                  message: "비밀번호를 입력해주세요!",
-                },
-                {
-                  validator: validateToNextPassword,
-                },
-              ],
-            })(<Input.Password />)}
-          </Form.Item>
-          <Form.Item label='비밀번호 확인' hasFeedback>
-            {getFieldDecorator("confirm", {
-              rules: [
-                {
-                  required: true,
-                  message: "비밀번호를 확인해주세요!",
-                },
-                {
-                  validator: compareToFirstPassword,
-                },
-              ],
-            })(<Input.Password onBlur={handleConfirmBlur} />)}
-          </Form.Item>
-          <Form.Item
-            label={
-              <span>
-                닉네임&nbsp;
-                <Tooltip title='무엇으로 불리고 싶으신가요?'>
-                  <Icon type='question-circle-o' />
-                </Tooltip>
-              </span>
-            }
-          >
-            {getFieldDecorator("nickname", {
-              rules: [
-                {
-                  required: true,
-                  message: "닉네임을 입력해주세요!",
-                  whitespace: true,
-                },
-              ],
-            })(<Input />)}
-          </Form.Item>
-          <Form.Item label='거주지'>
-            {getFieldDecorator("residence", {
-              initialValue: ["망원동", "성산동"],
-              rules: [
-                {
-                  type: "array",
-                  required: true,
-                  message: "거주지를 입력해주세요!",
-                },
-              ],
-            })(<Cascader options={residences} />)}
-          </Form.Item>
-          <Form.Item label='휴대전화'>
-            {getFieldDecorator("phone", {
-              rules: [{ required: true, message: "핸드폰 번호를 입력해주세요!" }],
-            })(<Input addonBefore={prefixSelector} style={{ width: "100%" }} />)}
-          </Form.Item>
-          <Form.Item {...tailFormItemLayout}>
-            {getFieldDecorator("agreement", {
-              valuePropName: "checked",
-              // rules: [{ validator: this.validateChecked, message: "약관에 동의하셔야합니다!" }]
-            })(
-              <Checkbox>
-                저는 <a href=''>약관</a>을 읽었고 동의합니다.
-              </Checkbox>,
-            )}
-          </Form.Item>
-          <Form.Item {...tailFormItemLayout}>
-            <Button type='primary' htmlType='submit' loading={isSigningUp}>
-              가입하기
-            </Button>
-          </Form.Item>
-        </Form>
-      </StyledForm>
-    </>
+    <Form
+      {...formItemLayout}
+      form={form}
+      name='register'
+      onFinish={onFinish}
+      initialValues={{
+        prefix: "82",
+      }}
+      scrollToFirstError
+    >
+      <Form.Item
+        name='email'
+        label='E-mail'
+        rules={[
+          {
+            type: "email",
+            message: "올바른 이메일을 입력해주세요!",
+          },
+          {
+            required: true,
+            message: "이메일을 입력해주세요!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name='password'
+        label='비밀번호'
+        rules={[
+          {
+            required: true,
+            message: "비밀번호를 입력해주세요!",
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name='confirm'
+        label='비밀번호 확인'
+        dependencies={["password"]}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "비밀번호 확인이 틀렸습니다!",
+          },
+          ({ getFieldValue }) => ({
+            validator(rule, value) {
+              if (!value || getFieldValue("password") === value) {
+                return Promise.resolve()
+              }
+              return Promise.reject("비밀번호가 일치하지 않습니다!")
+            },
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name='nickname'
+        label={
+          <span>
+            닉네임&nbsp;
+            <Tooltip title='닉네임을 정해주세요!'>
+              <QuestionCircleOutlined />
+            </Tooltip>
+          </span>
+        }
+        rules={[{ required: true, message: "닉네임을 입력해주세요!", whitespace: true }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name='phone'
+        label='전화번호'
+        rules={[{ required: true, message: "전화번호를 입력해주세요!" }]}
+      >
+        <Input addonBefore={prefixSelector} style={{ width: "100%" }} />
+      </Form.Item>
+
+      <Form.Item label='Captcha' extra='We must make sure that your are a human.'>
+        <Row gutter={8}>
+          <Col span={12}>
+            ㅎ
+            <Form.Item
+              name='captcha'
+              noStyle
+              rules={[{ required: true, message: "쓰여진 문자를 입력해주세요!" }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Button>Get captcha</Button>
+          </Col>
+          ㅎ
+        </Row>
+      </Form.Item>
+
+      <Form.Item name='agreement' valuePropName='checked' {...tailFormItemLayout}>
+        <Checkbox>
+          <a href=''>이용약관</a> 을 읽었으며 동의합니다.
+        </Checkbox>
+      </Form.Item>
+      <Form.Item {...tailFormItemLayout}>
+        <Button type='primary' htmlType='submit'>
+          회원가입하기
+        </Button>
+      </Form.Item>
+    </Form>
   )
 }
 
-export default memo(SignupForm)
+export default SignupForm
--- frontend/pages/signup.tsx
+++ frontend/pages/signup.tsx
@@ -1,14 +1,13 @@
 import React, { memo, useEffect } from "react"
 import { Form } from "antd"
 import Router from "next/router"
-
-const WrappedSignupForm = Form.useForm({ name: "register" })(SignupForm)
+import SignupForm from "../components/SignupForm"
 
 const me = { id: 1, nickname: "서상혁" }
 
 const Signup = () => {
   useEffect(() => {
-    if (me) {
+    if (!me) {
       alert("이미 로그인이 된 상태입니다!")
       Router.push("/")
     }
@@ -16,7 +15,7 @@ const Signup = () => {
   return (
     <>
       <div>회원가입</div>
-      <WrappedSignupForm />
+      <SignupForm />
     </>
   )
 }
