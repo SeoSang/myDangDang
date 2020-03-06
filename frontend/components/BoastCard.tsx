3fdf96db49f5bdf6cde87ea8757b0cc64b952efb
SeoSang <ddrrpg@naver.com>
2020-03-06 16:54:10 +0900

    카드 버그 수정

--- frontend/components/BoastCard.tsx
+++ frontend/components/BoastCard.tsx
@@ -6,10 +6,10 @@ const BoastCard = ({ card }) => {
   return (
     <Card
       hoverable
-      style={{ width: 240, maxHeight: 400, textAlign: "center", margin: "0 auto" }}
-      cover={<img style={{ maxHeight: 280 }} alt='example' src={card.img} />}
+      style={{ width: 250, maxHeight: 400, textAlign: "center", margin: "0 auto" }}
+      cover={<img style={{ width: 250, maxHeight: 250 }} alt='example' src={card.img} />}
     >
-      <Meta title={card.title} description={card.description} />
+      <Meta style={{ maxHeight: 150 }} title={card.title} description={card.description} />
     </Card>
   )
 }
